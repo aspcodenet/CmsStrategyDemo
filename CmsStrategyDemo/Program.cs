c526866a9eb01b81374c877e5458dd9253c441c6
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-11-26 13:44:58 +0100

    Builder

--- CmsStrategyDemo/Program.cs
+++ CmsStrategyDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CmsStrategyDemo
 {
@@ -134,10 +135,86 @@ namespace CmsStrategyDemo
 
         private ITaxCalculationStrategy GetTaxCalculationToUseForThisPerson()
         {
-            if(Country == "SE") return new SwedenTaxCalculation();
-            if (Country == "FI") return new FinlandTaxCalculation(Yrke);
-            if (Country == "NO") return new NorwayTaxCalculation(Yrke,Namn);
+            TaxCalculationFactory factory = new TaxCalculationFactory();
+            return factory.GetTaxCalculationToUseForThisPerson(Country, Yrke, Namn);
+        }
+    }
+
+    class TaxCalculationFactory
+    {
+        public ITaxCalculationStrategy GetTaxCalculationToUseForThisPerson(string Country, Yrke yrke, string Namn)
+        {
+            if (Country == "SE") return new SwedenTaxCalculation();
+            if (Country == "FI") return new FinlandTaxCalculation(yrke);
+            if (Country == "NO") return new NorwayTaxCalculation(yrke, Namn);
             throw new Exception("Fel land");
+        }
+    }
+
+
+    /*
+     *
+        ct
+      public SalesOrder(string customerName, string customerPhoneNumber, string customerEmailAddress,
+                string customerShippingAddress, List<string> lineItems, bool wrapAsGift
+                )
+            {
+                CustomerName = customerName;
+                CustomerPhoneNumber = customerPhoneNumber;
+                CustomerEmailAddress = customerEmailAddress;
+                CustomerShippingAddress = customerShippingAddress;
+                LineItems = lineItems;
+                WrapAsGift = wrapAsGift;
+            }
+     *
+     */
+
+
+    class SalesOrder
+    {
+        public string CustomerName{ get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string CustomerEmailAddress { get; set; }
+        public string CustomerShippingAddress { get; set; }
+        public List<string> LineItems { get; set; }
+        public bool WrapAsGift { get; set; }
+    }
+
+
+    class SalesOrderBuilder
+    {
+        public SalesOrderBuilder CustomerInfo(string customerName, string customerPhoneNumber, string customerEmailAddress)
+        {
+            CustomerName = customerName;
+            CustomerPhoneNumber = customerPhoneNumber;
+            CustomerEmailAddress = customerEmailAddress;
+            return this;
+        }
+        public SalesOrderBuilder WrapAsGift()
+        {
+            wrapAsGift = true;
+            return this;
+        }
+
+        string CustomerName { get; set; }
+        string CustomerPhoneNumber { get; set; }
+        string CustomerEmailAddress { get; set; }
+        string CustomerShippingAddress { get; set; }
+        List<string> LineItems { get; set; }
+        bool wrapAsGift { get; set; }
+
+        public SalesOrder Build()
+        {
+            var salesOrder = new SalesOrder
+            {
+                CustomerName = CustomerName,
+                CustomerPhoneNumber = CustomerPhoneNumber,
+                CustomerEmailAddress = CustomerEmailAddress,
+                CustomerShippingAddress = CustomerShippingAddress,
+                LineItems = LineItems,
+                WrapAsGift = wrapAsGift
+            };
+            return salesOrder;
         }
     }
 
@@ -146,6 +223,31 @@ namespace CmsStrategyDemo
     {
         static void Main(string[] args)
         {
+            var salesOrder2 = new SalesOrderBuilder()
+                .CustomerInfo("Mats Sundin", "555-12121212", "m@m.se")
+                //.SetLines()
+                .WrapAsGift()
+                .Build();
+
+            var salesOrder = new SalesOrder
+            {
+                CustomerName = "Mats Sundin",
+                CustomerPhoneNumber = "555 - 222 111",
+                CustomerEmailAddress = "mats@toronto.ca",
+                CustomerShippingAddress = "Maple Leafs Garden 123, CA 11122 Toronto",
+                LineItems = new List<string> { "Sticks"  },
+                WrapAsGift = true
+            };
+
+
+
+
+
+
+
+
+
+
             var p = new Person { Namn="Ole", Country="NO", Salary = 1000, Yrke = Yrke.OilWorker};
             var p2 = new Person { Namn = "Stefan", Country = "SE", Salary = 1000, Yrke = Yrke.Programmer };
             
